af261b0bddf31dac7bbe78683fc00231573f5636
Trevor Nowalk <trevor.nowalk@gmail.com>
2020-06-04 22:32:33 -0400

    Removed console

--- src/App.js
+++ src/App.js
@@ -116,10 +116,8 @@ function App() {
   useEffect(() => {
     try {
       let processed = processInput(JSON.parse(input));
-      console.log(processed);
       setOutput(buildOutput(processed).join("\n"));
     } catch (err) {
-      // console.log("Not valid JSON", err);
       setOutput("Please enter a valid JSON string");
     }
   }, [input]);
